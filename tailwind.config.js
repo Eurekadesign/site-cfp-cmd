7dd5e9a73f0bc6709a23d9beb542e48aa98990d0
Eurekadesign <eurekadesignkonsulting@gmail.com>
2023-11-10 12:58:42 +0100

    07 commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: '#0076D0',
+        darkGrayishBlue: '#0091FF',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      },
+    },
   },
   plugins: [],
 }
